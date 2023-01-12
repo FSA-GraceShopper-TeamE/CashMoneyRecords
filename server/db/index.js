e61007919679f2fcc1df6a4ccd4bd764c8327b3f
Ricardo <richardproject.us@gmail.com>
2023-01-12 10:43:44 -0600

    db update

--- server/db/index.js
+++ server/db/index.js
@@ -1,8 +1,11 @@
 //this is the access point for all things database related!
 
-const db = require('./db')
+const db = require("./db");
+const Order = require("./models/Order");
+const OrderAlbum = require("./models/OrderAlbum");
+const Album = require("./models/Album");
 
-const User = require('./models/User')
+const User = require("./models/User");
 
 //associations could go here!
 
@@ -10,5 +13,15 @@ module.exports = {
   db,
   models: {
     User,
+    Order,
+    OrderAlbum,
+    Album,
   },
-}
+};
+User.hasMany(Order);
+Order.belongsTo(User);
+
+Album.hasMany(OrderAlbum);
+OrderAlbum.belongsTo(Album);
+
+OrderAlbum.belongsTo(Order);
